397b5500880baaa77c110d8973b9d698547c1d9e
Aditya Singh <aditya@adityalaptop.(none)>
2014-07-03 13:17:20 +0530

    modified:   launch-all.sh

--- launch-all.sh
+++ launch-all.sh
@@ -2,3 +2,4 @@ python ad-server.py -port=8888 -name=Tornado1 -refreshCache=15000&
 python ad-server.py -port=8889 -name=Tornado2 -refreshCache=15000&
 python ad-server.py -port=8890 -name=Tornado3 -refreshCache=15000&
 python ad-server.py -port=8891 -name=Tornado4 -refreshCache=15000&
+python log-agent.py
